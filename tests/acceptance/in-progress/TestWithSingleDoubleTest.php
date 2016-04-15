9fd4b5ca2fe2a398e7646d7ddeb71699decc78b4
Conor Smith <conorjbsmith@gmail.com>
2016-04-15 10:14:29 +0100

    Created scaffolding for acceptance testing

---
+++ tests/acceptance/in-progress/TestWithSingleDoubleTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+class TestWithSingleDoubleTest extends AcceptanceTestCase
+{
+    /**
+     * @test
+     */
+    public function without_an_annotation()
+    {
+        //
+    }
+}
